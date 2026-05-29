a57e00f221c957fa73bab63eccbb991dbd487525
Manish Kumawat <manishkmt211@gmail.com>
2026-05-29 14:15:14 +0530

    First commit

---
+++ ractangleFillPattern/RactangleFill.c
@@ -0,0 +1,58 @@
+
+//! In C
+
+#include<stdio.h>
+
+
+//! to print the value of n.
+// int main(){
+
+//   int n = 5;
+//   for(int i = 0; i < n; i++)
+//   {
+//     for(int j = 0; j < n; j++)
+//     {
+//       printf(" * ");
+//     }
+//     printf("\n");
+//   }
+
+//   return 0;
+// }
+
+//! to print of the value of i
+
+// int main(){
+
+//   int n = 5;
+//   for(int i = 0; i < n; i++)
+//   {
+//     for(int j = 0; j < n; j++)
+//     {
+//       printf("%d",i);
+//       printf(" ");
+//     }
+//     printf("\n");
+//   }
+
+//   return 0;
+// }
+
+
+//! to print the value of j.
+
+int main(){
+
+  int n = 5;
+  for(int i = 0; i < n; i++)
+  {
+    for(int j = 0; j < n; j++)
+    {
+      printf("%d",j);
+      printf(" ");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
---
+++ ractangleFillPattern/rectangleFill.java
@@ -0,0 +1,58 @@
+
+//! In JAVA
+
+package ractangleFillPattern;
+
+//! to print the value of n.
+
+// class rectangleFill{
+  
+//   public static void main(String[] args)
+//   {
+//     for(int i = 0; i < 5; i++)
+//     {
+//       for(int j = 0; j < 5; j++){
+//         System.out.print(" * ");
+//     }
+//     System.out.println();
+//   }
+// }
+// }
+
+//! to print the value of i.
+
+// class rectangleFill{
+  
+//   public static void main(String[] args)
+//   {
+
+//     int n =5;
+//     for(int i = 0; i < n; i++)
+//     {
+//       for(int j = 0; j < n; j++){
+//         System.out.print( i);
+//         System.out.print(" ");
+//     }
+//     System.out.println();
+//   }
+// }
+// }
+
+
+//! to print the value of j.
+class rectangleFill{
+  
+  public static void main(String[] args)
+  {
+
+    int n =5;
+    for(int i = 0; i < n; i++)
+    {
+      for(int j = 0; j < n; j++){
+        System.out.print(j);
+        System.out.print(" ");
+    }
+    System.out.println();
+  }
+}
+}
