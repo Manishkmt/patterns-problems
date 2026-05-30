0b03bf9872fe818ef74e2e75de9082c3d93a6d4d
Manish Kumawat <manishkmt211@gmail.com>
2026-05-30 16:00:45 +0530

    Initial commit

---
+++ plus pattern/plus.c
@@ -0,0 +1,25 @@
+//!Print
+//     x
+//     x
+// x x x x x
+//     x
+//     x
+
+#include<stdio.h>
+int main(){
+  int n = 5;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      if(i == n/2 || j == n/2){
+        printf("X ");
+      }
+      else{
+        printf("  ");
+
+      }
+    }
+    printf("\n");
+
+  }
+  return 0;
+}
