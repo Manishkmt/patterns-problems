12b742fb82ad1b2e6b7e2045bfe386d436bfc0ac
Manish Kumawat <manishkmt211@gmail.com>
2026-06-04 06:28:03 +0530

    Initial Commit

--- ox-pattern/ox.java
+++ ox-pattern/ox.java
@@ -1,18 +1,17 @@
-
 public class ox {
-  public static void main(String[] args){
-    int n = 5;
-    for(int i = 1; i <= n; i++){
-      for(int j = 1; i <= n; j++){
+    public static void main(String[] args) {
+        int n = 5;
 
-        if(i == (n/2)+1 || j == (n/2)+1){
-          System.out.println("X ");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+
+                if(i == (n/2)+1 || j == (n/2)+1) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("O ");
+                }
+            }
+            System.out.println();
         }
-        else{
-          System.out.println("O ");
-        }
-      }
-      System.out.println();
     }
-  }
 }
